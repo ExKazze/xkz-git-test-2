9771783b845ffb4ad6f9643c303831e3d2b06ac3
exkazze <exkazze@gmail.com>
2021-01-24 10:43:05 +0700

    menambahkan list di index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In architecto natus assumenda ex quasi sunt fuga aliquid nihil minus nobis dolorem, corrupti eius, autem numquam tempora rerum cum. Deleniti, aliquid?</p>
+    <ul>
+        <li>ichi</li>
+        <li>nii</li>
+    </ul>
 </body>
 </html>
